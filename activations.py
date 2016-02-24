c5c1adc653021882f7257d3a2d4a8707a2a24af7
musyoku <musyoku@users.noreply.github.com>
2016-02-24 22:30:56 +0900

    a

---
+++ activations.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import chainer.functions as F
+
+activations = {
+	"sigmoid": F.sigmoid, 
+	"tanh": F.tanh, 
+	"softplus": F.softplus, 
+	"relu": F.relu, 
+	"leaky_relu": F.leaky_relu, 
+	"elu": F.elu
+}
