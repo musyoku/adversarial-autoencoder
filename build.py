a32ebb1a1264899aa8eeac41d5284fd1cf1f3c68
musyoku <musyoku@users.noreply.github.com>
2016-03-14 02:29:27 +0900

    j

--- build.py
+++ build.py
@@ -14,14 +14,14 @@ def build(config):
 
 	if config.gen_encoder_type == "deterministic":
 		for i, (n_in, n_out) in enumerate(gen_layer_units):
-			gen_attributes["layer_%i" % i] = L.Linear(n_in, n_out, wscale=config.wscale_base * math.sqrt(n_in * n_out))
+			gen_attributes["layer_%i" % i] = L.Linear(n_in, n_out, wscale=config.wscale)
 			gen_attributes["batchnorm_%i" % i] = L.BatchNormalization(n_out)
 
 	elif config.gen_encoder_type == "gaussian":
 		for i, (n_in, n_out) in enumerate(gen_layer_units):
-			gen_attributes["layer_mean_%i" % i] = L.Linear(n_in, n_out, wscale=config.wscale_base * math.sqrt(n_in * n_out))
+			gen_attributes["layer_mean_%i" % i] = L.Linear(n_in, n_out, wscale=config.wscale)
 			gen_attributes["batchnorm_mean_%i" % i] = L.BatchNormalization(n_out)
-			gen_attributes["layer_variance_%i" % i] = L.Linear(n_in, n_out, wscale=config.wscale_base * math.sqrt(n_in * n_out))
+			gen_attributes["layer_variance_%i" % i] = L.Linear(n_in, n_out, wscale=config.wscale)
 			gen_attributes["batchnorm_variance_%i" % i] = L.BatchNormalization(n_out)
 
 	gen = Generator(**gen_attributes)
@@ -41,7 +41,7 @@ def build(config):
 	dis_layer_units += [(config.n_dis_hidden_units[-1], 2)]
 
 	for i, (n_in, n_out) in enumerate(dis_layer_units):
-		dis_attributes["layer_%i" % i] = L.Linear(n_in, n_out, wscale=config.wscale_base * math.sqrt(n_in * n_out))
+		dis_attributes["layer_%i" % i] = L.Linear(n_in, n_out, wscale=config.wscale)
 		dis_attributes["batchnorm_%i" % i] = L.BatchNormalization(n_out)
 
 	dis = Discriminator(**dis_attributes)
@@ -59,7 +59,7 @@ def build(config):
 	dec_layer_units += [(config.n_dec_hidden_units[-1], config.n_x)]
 
 	for i, (n_in, n_out) in enumerate(dec_layer_units):
-		dec_attributes["layer_%i" % i] = L.Linear(n_in, n_out, wscale=config.wscale_base * math.sqrt(n_in * n_out))
+		dec_attributes["layer_%i" % i] = L.Linear(n_in, n_out, wscale=config.wscale)
 		dec_attributes["batchnorm_%i" % i] = L.BatchNormalization(n_out)
 
 	dec = Decoder(**dec_attributes)
